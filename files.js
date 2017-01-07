5c0dc20b23db21ce827d5b67eb9523727f93b4d8
Erick Rivas <erivas@birkman.com>
2017-01-06 20:05:48 -0600

    Begin files module

---
+++ files.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var nodePath = require('path');
+
+var files = module.exports = {
+  /**
+   * Returns the contents of the file at the given path as a string.
+   * If the file does not exist, it returns undefined.
+   *
+   * @param path
+   */
+  read: function(path) {
+    if (fs.existsSync(path)) {
+      return fs.readFileSync(path, "utf8");
+    }
+  },
+  /**
+   * Returns a string by concatenating the given path to the absolute path of the .gitlet directory
+   *
+   * @param path
+   */
+  gitletPath: function(path) {
+    function gitletDir(dir) {
+      if (fs.existsSync(dir)) {
+        var potentialConfigFile = nodePath.join(dir, 'config');
+        var potentialGitletPath = nodePath.join(dir, '.gitlet');
+
+        if (fs.existsSync(potentialConfigFile) &&
+            fs.statSync(potentialConfigFile).isFile() &&
+            files.read(potentialConfigFile).match(/\[core\]/)) {
+          return dir;
+        }
+        else if (fs.existsSync(potentialGitletPath)) {
+          return potentialGitletPath;
+        }
+        else if ("/" !== dir) {
+          return gitletDir(nodePath.join(dir, ".."));
+        }
+      }
+
+      var gitDir = gitletDir(process.cwd());
+      if (gitDir !== undefined) {
+        return nodePath.join(gitDir, path || "");
+      }
+    }
+  },
+  /**
+   * Check if the current working directory is inside a repo
+   *
+   * @returns {boolean}
+   */
+  inRepo: function() {
+    return files.gitletPath() !== undefined;
+  }
+};
